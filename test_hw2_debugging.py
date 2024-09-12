51003d63b3776e00438a50181ba71ba6f3c4d5a9
Gwen Mason <96750720+gwenmason125@users.noreply.github.com>
2024-09-12 01:02:06 +0000

    fixed formattingof tests for pylint

--- test_hw2_debugging.py
+++ test_hw2_debugging.py
@@ -1,22 +1,28 @@
-# test_hw2_debugging.py
-import pytest
-from hw2_debugging import merge_sort
+'''
+Tests the merge sort
+'''
 
-# test to verify sorting a list of integer
+# test_hw2_debugging.py
+from hw2_debugging import merge_sort
 
 
 def test_sort_integers():
+    '''
+    test to verify sorting a list of integer
+    '''
     assert merge_sort([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]) == [
         1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
 
-# test to verify sorting an empty list
-
 
 def test_sort_empty_list():
-    assert merge_sort([]) == []
-
-# test to verify sorting a list of duplicate values
+    '''
+    test to verify sorting an empty list
+    '''
+    assert not merge_sort([])
 
 
 def test_sort_duplicates():
+    '''
+    test to verify sorting a list of duplicate values
+    '''
     assert merge_sort([5, 5, 5, 5]) == [5, 5, 5, 5]
