321472b2377693c06aad56270fa67cb618fcc5a2
Gwen Mason <96750720+gwenmason125@users.noreply.github.com>
2024-09-12 00:19:37 +0000

    fixed merge sort

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -9,38 +9,46 @@ def merge_sort(arr_to_sort):
     '''
     Sorts the given array using merge sort
 
-    @arr the array to be sorted
+    @arr_to_sort: The array to be sorted
     '''
-    if len(arr_to_sort) == 1:
+    if len(arr_to_sort) <= 1:  # Base case should handle empty and single-element arrays
         return arr_to_sort
 
-    half = len(arr_to_sort)//2
+    half = len(arr_to_sort) // 2
 
-    return recombine(merge_sort(arr_to_sort[:half]), merge_sort(arr_to_sort[half:]))
+    # Recursively split and merge
+    left_sorted = merge_sort(arr_to_sort[:half])
+    right_sorted = merge_sort(arr_to_sort[half:])
+
+    return recombine(left_sorted, right_sorted)
 
 
 def recombine(left_arr, right_arr):
     '''
     Used by merge sort to sort the list
-    @leftArr the left array to be combined
-    @rightArray the right array to be combined
+    @left_arr: the left array to be combined
+    @right_arr: the right array to be combined
     '''
     left_index = 0
     right_index = 0
-    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    merge_arr = []
+
+    # Merge the two arrays
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
-            right_index += 1
-            merge_arr[left_index + right_index] = left_arr[left_index]
-        else:
+            merge_arr.append(left_arr[left_index])
             left_index += 1
-            merge_arr[left_index + right_index] = right_arr[right_index]
+        else:
+            merge_arr.append(right_arr[right_index])
+            right_index += 1
 
-    for i in range(right_index, len(right_arr)):
-        merge_arr[left_index + right_index] = right_arr[i]
+    while left_index < len(left_arr):
+        merge_arr.append(left_arr[left_index])
+        left_index += 1
 
-    for i in range(left_index, len(left_arr)):
-        merge_arr[left_index + right_index] = left_arr[i]
+    while right_index < len(right_arr):
+        merge_arr.append(right_arr[right_index])
+        right_index += 1
 
     return merge_arr
 
