04a7a664c591401b563f58c3e9cc0ff37ee56ad9
Gwen Mason <96750720+gwenmason125@users.noreply.github.com>
2024-09-12 00:55:18 +0000

    running actions on all py files

--- test-error.py
+++ test-error.py
@@ -2,12 +2,16 @@
 import pytest
 
 # Passing test
+
+
 def test_syntax_error():
     with pytest.raises(TypeError):
         # Importing the file should raise a TypeError due to the last line
         __import__("error")
-        
+
 # Failing test, the program should not be able to print out "3" because of the type error
+
+
 def test_correct_lines(capsys):
     # Test the console output
     captured = capsys.readouterr()
--- test_hw2_debugging.py
+++ test_hw2_debugging.py
@@ -3,13 +3,20 @@ import pytest
 from hw2_debugging import merge_sort
 
 # test to verify sorting a list of integer
+
+
 def test_sort_integers():
-    assert merge_sort([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]) == [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
+    assert merge_sort([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]) == [
+        1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
 
 # test to verify sorting an empty list
+
+
 def test_sort_empty_list():
     assert merge_sort([]) == []
 
 # test to verify sorting a list of duplicate values
+
+
 def test_sort_duplicates():
     assert merge_sort([5, 5, 5, 5]) == [5, 5, 5, 5]
